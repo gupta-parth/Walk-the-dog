1f5cd6083aa70a9cc8a66012ad2d240118d2179c
Parth Gupta <parthgupta023@gmail.com>
2021-09-18 13:47:41 -0400

    Modified files

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import flash, redirect, render_template, session, request, jsonify, make_response, url_for
+from functools import wraps
+
+from werkzeug.exceptions import MethodNotAllowed
 
 app = Flask(__name__)
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+    
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
 
 @app.route("/")
 def index():
